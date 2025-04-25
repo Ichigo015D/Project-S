b8482ab1f45242ff883c472dca10072b962ec654
Ichigo015D <satriawinata480@gmail.com>
2025-04-25 21:03:31 +0200

    Update Tipo.java

--- Binary treeeeeeeee/Tipo.java
+++ Binary treeeeeeeee/Tipo.java
@@ -1,4 +1,4 @@
-public class Tipo { // devo fare anche questo clase, perchè se no non funziona tipo nel main 
+public class Tipo { // devo fare anche questo clase, perchè se no non funziona tipo nel Alberobinario
     private int valore;
 
     public Tipo(int valore) {
